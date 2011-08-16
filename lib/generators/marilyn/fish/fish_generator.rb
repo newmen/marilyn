8b3526ec4a352f3f83ae00ac2a5e1394cb74e5fa
Gleb Averchuk <altermn@gmail.com>
2011-08-16 18:57:46 +0400

    fix paths

--- lib/generators/marilyn/fish/fish_generator.rb
+++ lib/generators/marilyn/fish/fish_generator.rb
@@ -12,20 +12,20 @@ module Marilyn
       def replace_application_helper
         app_helper_file = 'app/helpers/application_helper.rb'
         remove_file(app_helper_file)
-        copy_file('helpers/application_helper.rb', app_helper_file)
+        copy_file('app/helpers/application_helper.rb', app_helper_file)
       end
 
       def replace_layouts
         app_layout_file = 'app/views/layouts/application.html.erb'
         remove_file(app_layout_file)
-        copy_file('layouts/application.html.erb', app_layout_file)
-        copy_file('layouts/_form_errors.html.erb', 'app/views/layouts/_form_errors.html.erb')
+        copy_file('app/layouts/application.html.erb', app_layout_file)
+        copy_file('app/layouts/_form_errors.html.erb', 'app/views/layouts/_form_errors.html.erb')
       end
 
       def replace_locales
         remove_file('config/locales/en.yml')
-        copy_file('locales/en.yml', 'config/locales/en.yml')
-        copy_file('locales/ru.yml', 'config/locales/ru.yml')
+        copy_file('app/locales/en.yml', 'config/locales/en.yml')
+        copy_file('app/locales/ru.yml', 'config/locales/ru.yml')
       end
     end
   end
